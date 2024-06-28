3a4944c4d92929014b00cd49b6bfa50930c26578
No Hope <haov03200@gmail.com>
2024-06-28 11:57:34 +0700

    feedback

--- DemoSanBong/Controllers/FeedBackController.cs
+++ DemoSanBong/Controllers/FeedBackController.cs
@@ -1,6 +1,8 @@
 ﻿using DemoSanBong.Models;
+using DemoSanBong.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Differencing;
 using System.Security.Claims;
 
 namespace DemoSanBong.Controllers
@@ -14,33 +16,112 @@ namespace DemoSanBong.Controllers
         {
             _Context = context;
         }
-        [HttpPost]
-        public async Task<IActionResult> Create (FeedBack model)
+        public IActionResult Create ()
         {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-            if(_Context.FeedBacks.Any(f => f.CusId == userId))
+            return View();
+        }
+        [Authorize(Roles = "Customer")]
+        [HttpPost]
+        public async Task<IActionResult> Create (CreateFeedbackViewModel model)
+        {
+            if (ModelState.IsValid)
             {
-                return BadRequest("Bạn đã để lại feed back");
+                var feedback = new FeedBack
+                {
+                    CusId = model.CusId,
+                    Stars = model.Stars,
+                    Commment = model.Commment,
+                    CreateDate = DateTime.Now,
+                    UpdateDate = DateTime.Now,
+                    IsShow = true
+                };
+                _Context.FeedBacks.Add(feedback);
+                await _Context.SaveChangesAsync();
+                return RedirectToAction("Index");
             }
-
-            var feedback = new FeedBack { 
-                CusId = userId,
-                Stars = model.Stars,
-                Commment = model.Commment,
-                CreateDate = DateTime.Now,
-                UpdateDate = DateTime.Now,
-                IsShow = true
-            };
-            _Context.FeedBacks.Add(feedback);
+            return View(model);           
+        }
+        [Authorize(Roles = "Customer")]
+        [HttpPost]
+        public async Task<IActionResult> Update(EditFeedbackViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var fb = _Context.FeedBacks.FirstOrDefault(i => i.CusId == model.CusId);
+                if (fb == null)
+                {
+                    return NotFound();
+                }
+                fb.Stars = model.Stars;
+                fb.Commment = model.Commment;
+                fb.UpdateDate = DateTime.Now;
+                fb.IsShow = model.IsShow;
+                _Context.FeedBacks.Update(fb);
+                await _Context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        [Authorize(Roles = "Customer")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var fb = _Context.FeedBacks.FirstOrDefault(i => i.CusId == id);
+            if (fb == null)
+            {
+                return NotFound();
+            }
+            _Context.FeedBacks.Remove(fb);
             await _Context.SaveChangesAsync();
             return RedirectToAction("Index");
-
+        }
+        [Authorize(Roles = "Customer")]
+        public IActionResult Details(string id)
+        {
+            var detail = _Context.FeedBacks.FirstOrDefault(i => i.CusId == id);
+            if (detail == null)
+            {
+                return NotFound();
+            }
+            var details = new EditFeedbackViewModel()
+            {
+                CusId = detail.CusId,
+                Stars = detail.Stars,
+                Commment = detail.Commment,
+                IsShow = detail.IsShow,
+            };
+            return View(details);
+        }
+        [Authorize (Roles ="Admin")]
+        public async Task<IActionResult> Hide(string id)
+        {
+            var fb = _Context.FeedBacks.FirstOrDefault(i => i.CusId == id);
+            if (fb == null)
+            {
+                return NotFound();
+            }
+            fb.IsShow = false;
+            _Context.FeedBacks.Update(fb);
+            await _Context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Index()
         {
-            return View();
+            var list =_Context.FeedBacks.ToList();
+            var models = new List<EditFeedbackViewModel>();
+            foreach (var fb in list)
+            {
+                var model = new EditFeedbackViewModel
+                {
+                    CusId = fb.CusId,
+                    Stars = fb.Stars,
+                    Commment = fb.Commment,
+                    IsShow = fb.IsShow,
+                };
+                models.Add(model);
+            }
+            return View(models);
         }
+
     }
 }
---
+++ DemoSanBong/ViewModels/CreateFeedbackViewModel.cs
@@ -0,0 +1,18 @@
+﻿using DemoSanBong.Models;
+
+namespace DemoSanBong.ViewModels
+{
+    public class CreateFeedbackViewModel
+    {
+        public string CusId { get; set; }
+        public int Stars { get; set; }
+        public string Commment { get; set; }
+    }
+    public class EditFeedbackViewModel
+    {
+        public string CusId { get; set; }
+        public int Stars { get; set; }
+        public string Commment { get; set; }
+        public bool IsShow { get; set; }
+    }
+}
